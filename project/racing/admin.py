8a7db0092dbc3b592c15459a33f81abe6585e961
Ysowjanya9 <sowjanya.yeluri@capgemini.com>
2025-05-03 13:50:40 +0530

    Delete project directory

--- project/racing/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from . import models
-
-# Register your models here.
-
-admin.site.register(models.Team)
-admin.site.register(models.Driver)
-admin.site.register(models.Race)
