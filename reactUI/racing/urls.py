79032410b8129e211acc3ff76eedd25ada99a323
Ysowjanya9 <sowjanya.yeluri@capgemini.com>
2025-06-06 12:27:14 +0530

    Add files via upload

--- reactUI/racing/urls.py
+++ reactUI/racing/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     path('',views.home, name='home'),
     
-    # Team
+   # Team
    path('racing/teams/', views.team_list, name='team_list'),
    path('racing/teams/create/', views.team_create, name='team_create'),
    path('racing/teams/edit/<int:pk>/', views.team_edit, name='team_edit'),
@@ -21,35 +21,21 @@ urlpatterns = [
    path('racing/races/create/', views.race_create, name='race_create'),
    path('racing/races/edit/<int:pk>/', views.race_edit, name='race_edit'),
    path('racing/races/delete/<int:pk>/', views.race_delete, name='race_delete'),
+
    # Team API
-   path('racing/api/teams/create/', views.team_create_api.as_view()),
-   path('racing/api/teams/', views.team_list_api.as_view()),
-   path('racing/api/teams/edit/<int:pk>/', views.team_edit_api.as_view()),
-   path('racing/api/teams/delete/<int:pk>/', views.team_delete_api.as_view()),
-   # Driver API
-   path('racing/api/drivers/create/', views.driver_create_api.as_view()),
-   path('racing/api/drivers/', views.driver_list_api.as_view()),
-   path('racing/api/drivers/edit/<int:pk>/', views.driver_edit_api.as_view()),
-   path('racing/api/drivers/delete/<int:pk>/', views.driver_delete_api.as_view()),
-   # Race API
-   path('racing/api/races/create/',views.race_create_api.as_view()),
-   path('racing/api/races/',views.race_list_api.as_view()),
-   path('racing/api/races/edit/<int:pk>/',views.race_edit_api.as_view()),
-   path('racing/api/races/delete/<int:pk>/',views.race_delete_api.as_view()),
-   
-    # Team API
    path('racing/api/teams/create/', views.team_create_api.as_view()), # POST
    path('racing/api/teams/', views.team_list_api.as_view()), # GET
    path('racing/api/teams/edit/<int:pk>/', views.team_edit_api.as_view()), # GET, PUT, PATCH
    path('racing/api/teams/delete/<int:pk>/', views.team_delete_api.as_view()), # DELETE
-   # Driver API 
-   path('racing/api/drivers/create/',views.driver_create_api.as_view()), # POST
-   path('racing/api/drivers/',views.driver_list_api.as_view()), # GET
-   path('racing/api/drivers/edit/<int:pk>/',views.driver_edit_api.as_view()), # GET, PUT, PATCH
-   path('racing/api/drivers/delete/<int:pk>/',views.driver_delete_api.as_view()), # DELETE
+   # Driver API
+   path('racing/api/drivers/create/', views.driver_create_api.as_view()), # POST
+   path('racing/api/drivers/', views.driver_list_api.as_view()), # GET
+   path('racing/api/drivers/edit/<int:pk>/', views.driver_edit_api.as_view()), # GET, PUT, PATCH
+   path('racing/api/drivers/delete/<int:pk>/', views.driver_delete_api.as_view()), # DELETE
    # Race API
-   path('racing/api/races/create/', views.race_create_api.as_view()), # POST
-   path('racing/api/races/', views.race_list_api.as_view()), # GET
-   path('racing/api/races/edit/<int:pk>/', views.race_edit_api.as_view()), # GET, PUT, PATCH
-   path('racing/api/races/delete/<int:pk>/', views.race_delete_api.as_view()), # DELETE
+   path('racing/api/races/create/',views.race_create_api.as_view()), # POST
+   path('racing/api/races/',views.race_list_api.as_view()), # GET
+   path('racing/api/races/edit/<int:pk>/',views.race_edit_api.as_view()), # GET, PUT, PATCH
+   path('racing/api/races/delete/<int:pk>/',views.race_delete_api.as_view()), # DELETE
+   
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
